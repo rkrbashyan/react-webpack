15be19ac8d670340322710449a99887045730343
Ruben Krbashyan <krbashyan.r@gmai.com>
2018-03-17 22:25:08 +0200

    webpack setup for react project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Adding polyfill for IE (to support Promisses, Object.assign(), etc...)
+import 'babel-polyfill';
+
+import classes from './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+const app = (
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
